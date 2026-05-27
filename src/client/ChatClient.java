dd34ff846140a6b846ba5c0c19adec0f6c06ec7d
Deep Mitra <137201089+Jupiter-2003@users.noreply.github.com>
2026-05-27 13:11:13 +0530

    Added bidirectional communication between client and server.

--- src/client/ChatClient.java
+++ src/client/ChatClient.java
@@ -1,6 +1,9 @@
 package client;
 
 import common.Constants;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class ChatClient {
@@ -11,6 +14,30 @@ public class ChatClient {
             
             Socket socket = new Socket("localhost", Constants.PORT);
             System.out.println("Connected to server!");
+
+            BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
+            BufferedReader serverReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+
+            String userInput, response;
+
+            while(true){
+
+                System.out.print("You: ");
+                userInput = consoleReader.readLine();
+
+                writer.println(userInput);
+
+                if(userInput.equalsIgnoreCase("exit")){
+                    System.out.println("Disconnected from server!");
+                    break;
+                }
+
+                response = serverReader.readLine();
+                System.out.println("Server: " + response);
+            }
+
+            socket.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
--- src/server/ChatServer.java
+++ src/server/ChatServer.java
@@ -1,6 +1,9 @@
 package server;
 
 import common.Constants;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -15,6 +18,23 @@ public class ChatServer {
 
             Socket socket = serverSocket.accept(); //program waits here till client tries to connect
             System.out.println("Client connected!");
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream())); //to read msg from client
+            PrintWriter writer = new PrintWriter(socket.getOutputStream(), true); //to send msg to client
+
+            String message;
+
+            while((message = reader.readLine()) != null){
+                System.out.println("Client: " + message);
+                if(message.equalsIgnoreCase("exit")){
+                    System.out.println("Client disconnected.");
+                    break;
+                }
+                writer.println("Message received.");
+            }
+
+            socket.close();
+            serverSocket.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
